d0105874bb480404888c5dde12022d88f7df49b3
Justin Blankenship <jblanken@unca.edu>
2017-04-19 00:21:14 -0400

    Half-fixed search icon.  New javascript files for navigation and mapping.  New background image carousel using javascript.  New cat.  Still Ugly.  Minor styling.

---
+++ scripts/maps.js
@@ -0,0 +1,14 @@
+function myMap() {
+  myCenter=new google.maps.LatLng(35.616532, -82.567012);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
